ff3f093d2913730213b6567eb6c9f544f19cbc5c
Andrew Archibald <andrew@aarchibald.com>
2018-08-15 20:59:44 -0700

    Include complete code from Eloquent JavaScript

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import { PixelEditor, Picture, ToolSelect, ColorSelect, updateState, draw, fill, rectangle, pick } from './editor/PixelEditor';
+import { startPixelEditor } from './editor/editor';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    document.getElementById('editor').appendChild(startPixelEditor());
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,17 +18,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <PixelEditor
-          tool="draw"
-          color="#000000"
-          picture={Picture.empty(60, 30, '#f0f0f0')}
-          tools={{ draw, fill, rectangle, pick }}
-          controls={[ToolSelect, ColorSelect]}
-          dispatch={action => {
-            //state = updateState(state, action);
-            //app.syncState(state);
-          }}
-        />
+        <div id="editor"></div>
       </div>
     );
   }
